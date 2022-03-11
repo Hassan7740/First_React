57372441e98ac04844a5dd4560e9aef3f618e0f3
Hassan7740 <101173691+Hassan7740@users.noreply.github.com>
2022-03-11 14:35:43 +0200

    warming up

---
+++ movie/src/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import style from './App.module.css'
+export default function App() {
+  return (
+    <div>
+       <NavBar/>
+    </div>
+  )
+}
---
+++ movie/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App/App';
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { Router, Routes } from 'react-router-dom';
+
+ReactDOM.render(
+
+    <App />
+  ,
+  document.getElementById('root')
+);
